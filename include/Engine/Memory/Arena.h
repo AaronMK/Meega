c1a86aa689fbc804e152debbce0cb26f71e40926
Aaron Karp <aaron.m.karp@gmail.com>
2015-07-16 02:28:37 -0400

    - Made Arena a header only file.

--- include/Engine/Memory/Arena.h
+++ include/Engine/Memory/Arena.h
@@ -6,16 +6,14 @@
 #include <atomic>
 #include <memory>
 
+#include <malloc.h>
+
 namespace Engine
 {
-	/**
-	 * @todo
-	 *    Make additions lock-free and thread safe.
-	 */
-	class ENGINE_DYNAMIC_CLASS Arena
+	class Arena
 	{
 	private:
-		class ENGINE_DYNAMIC_CLASS EntryBase
+		class EntryBase
 		{
 		public:
 			EntryBase();
@@ -73,6 +71,38 @@ namespace Engine
 
 	/////////////////////////////////////////////
 
+	Arena::Arena(size_t byteSize)
+	{
+		mMemory = (unsigned char*)_aligned_malloc(16, mAllocSize);
+		mStackMarker = mMemory;
+
+		mAllocSize = byteSize;
+		mRemainingSpace = mAllocSize;
+
+		mLastAdded = nullptr;
+		mFirstAdded = nullptr;
+	}
+
+	Arena::Arena(Arena&& other)
+	{
+		mMemory = other.mMemory;
+		mStackMarker = other.mStackMarker;
+
+		mAllocSize = other.mAllocSize;
+		mRemainingSpace = other.mRemainingSpace;
+
+		mLastAdded = other.mLastAdded;
+		mFirstAdded = other.mFirstAdded;
+
+		other.mMemory = nullptr;
+	}
+
+	Arena::~Arena()
+	{
+		clear();
+		_aligned_free(mMemory);
+	}
+
 	template <typename T, typename... Args>
 	T* Arena::Push(Args&& ...args)
 	{
@@ -108,6 +138,54 @@ namespace Engine
 		}
 
 		return nullptr;
+	}
+
+	void Arena::clear()
+	{
+		if (nullptr == mMemory)
+			return;
+
+		EntryBase* entryPtr = mFirstAdded;
+		while (entryPtr)
+		{
+			EntryBase* nextPtr = entryPtr->mNext;
+
+			entryPtr->~EntryBase();
+			entryPtr = nextPtr;
+		}
+
+		mStackMarker = mMemory;
+		mRemainingSpace = mAllocSize;
+		mLastAdded = nullptr;
+		mFirstAdded = nullptr;
+	}
+
+	Arena& Arena::operator=(Arena&& other)
+	{
+		mMemory = other.mMemory;
+		mStackMarker = other.mStackMarker;
+
+		mAllocSize = other.mAllocSize;
+		mRemainingSpace = other.mRemainingSpace;
+
+		mLastAdded = other.mLastAdded;
+		mFirstAdded = other.mFirstAdded;
+
+		other.mMemory = nullptr;
+
+		return *this;
+	}
+
+	//////////////////////////////////////////////
+
+	Arena::EntryBase::EntryBase()
+	{
+		mNext = nullptr;
+	}
+
+	Arena::EntryBase::~EntryBase()
+	{
+
 	}
 
 	//////////////////////////////////////////////
