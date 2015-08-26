316d5e7f35aa59e856f9eff7a9cb8348bd7363bf
Aaron Karp <aaron.m.karp@gmail.com>
2015-08-26 03:28:21 -0400

    Add atumatic inclusion of Qt libraries for DevSupport config header.

--- include/Engine/DevSupport/DevSupport.h
+++ include/Engine/DevSupport/DevSupport.h
@@ -1,6 +1,22 @@
 #ifndef _ENGINE_DEV_SUPPORT_H_
 #define _ENGINE_DEV_SUPPORT_H_
 
+#include <Engine/Config.h>
 
+#ifdef ENGINE_DEVEOPMENT_SUPPORT
+
+#if defined(_WIN32)
+#	if defined(_DEBUG)
+#		pragma comment(lib, "Qt5Guid.lib")
+#		pragma comment(lib, "Qt5Cored.lib")
+#		pragma comment(lib, "Qt5Widgetsd.lib")
+#	else
+#		pragma comment(lib, "Qt5Gui.lib")
+#		pragma comment(lib, "Qt5Core.lib")
+#		pragma comment(lib, "Qt5Widgets.lib")
+#	endif
+#endif
+
+#endif // ENGINE_DEVEOPMENT_SUPPORT
 
 #endif // _ENGINE_DEV_SUPPORT_H_
