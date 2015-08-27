e42dc1a75096db70823e94830becbca9fe08829f
Aaron Karp <aaron.m.karp@gmail.com>
2015-08-27 02:29:31 -0400

    Fix signature of GLDebug callback.

--- src/Engine/Tasking/GpuPipeline.cpp
+++ src/Engine/Tasking/GpuPipeline.cpp
@@ -101,7 +101,7 @@ namespace Engine
 #	else
 		static bool debugProfile = false;
 
-		static void APIENTRY DebugGLCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, GLvoid* userParam)
+		static void APIENTRY DebugGLCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
 		{
 		}
 #	endif
