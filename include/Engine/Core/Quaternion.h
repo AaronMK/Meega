8e4d5d8cfdb5166a1a0473452bc17cd3d653733b
Aaron Karp <aaron.m.karp@gmail.com>
2015-09-04 03:52:12 -0400

    Hopefully fixed Quaternion.

--- include/Engine/Core/Quaternion.h
+++ include/Engine/Core/Quaternion.h
@@ -18,7 +18,7 @@ namespace Engine
 	public:
 		Quaternion();
 		Quaternion(const Quaternion &Q);
-		Quaternion(vec3 axis, float thetaRadians);
+		Quaternion(const vec3 &v, float w = 1.0);
 
 		Quaternion operator+(const Quaternion &Other) const;
 		Quaternion& operator+=(const Quaternion &Other);
@@ -29,7 +29,7 @@ namespace Engine
 		Quaternion operator/(float f) const;
 		Quaternion& operator/=(float f);
 
-		Quaternion Normalize() const;
+		Quaternion normalize() const;
 
 		/**
 		 * @todo
@@ -41,126 +41,9 @@ namespace Engine
 		static Quaternion slerp(const Quaternion &Q1, const Quaternion &Q2, float t);
 
 	private:
-		Quaternion(const vec4 &V);
-
-		vec4 qVector;
+		vec3 mVec;
+		float mW;
 	};
-
-	////////////////// Implementation //////////////////
-	
-	Quaternion::Quaternion()
-	: qVector(0.0f, 0.0f, 0.0f, 1.0f)
-	{
-	}
-
-	Quaternion::Quaternion(const vec4 &V)
-	: qVector(V)
-	{
-	}
-
-	Quaternion::Quaternion(const Quaternion &Other)
-	: qVector(Other.qVector)
-	{
-	}
-
-	Quaternion::Quaternion(vec3 axis, float thetaRadians)
-	{
-	}
-
-	Quaternion Quaternion::operator+(const Quaternion &Other) const
-	{
-		Quaternion ret(*this);
-		ret.qVector += Other.qVector;
-		return ret;
-	}
-
-	Quaternion& Quaternion::operator+=(const Quaternion &Other)
-	{
-		qVector += Other.qVector;
-		return *this;
-	}
-
-	Quaternion Quaternion::operator-(const Quaternion &Other) const
-	{
-		Quaternion ret(*this);
-		ret.qVector -= Other.qVector;
-		return ret;
-	}
-
-	Quaternion& Quaternion::operator-=(const Quaternion &Other)
-	{
-		qVector -= Other.qVector;
-		return *this;
-	}
-
-	Quaternion Quaternion::operator*(float f) const
-	{
-		Quaternion ret(*this);
-		ret.qVector *= f;
-		return ret;
-	}
-
-	Quaternion& Quaternion::operator*=(float f)
-	{
-		qVector *= f;
-		return *this;
-	}
-
-	Quaternion Quaternion::operator/(float f) const
-	{
-		Quaternion ret(*this);
-		ret.qVector /= f;
-		return ret;
-	}
-
-	Quaternion& Quaternion::operator/=(float f)
-	{
-		qVector /= f;
-		return *this;
-	}
-
-	Quaternion Quaternion::Normalize() const
-	{
-		return Quaternion( qVector/sqrtf(dot(qVector, qVector)) );
-	}
-
-	Transform Quaternion::toTransform() const
-	{
-		vec4 sq = qVector*qVector;
-		
-		float xx = sq[0], yy = sq[1], zz = sq[2];
-		float xy = qVector[0]*qVector[1], xz = qVector[0]*qVector[2], yz = qVector[1]*qVector[2];
-		float wx = qVector[0]*qVector[3],   wy = qVector[1]*qVector[3],   wz = qVector[2]*qVector[3];
-
-		mat4x4 transMat( 1 - 2*(yy + zz), 2*(xy + wz), 2*(xz - wy),  0.0f,
-		                 1 - 2*(xy + wz), 2*(xx + zz), 2*(yz - wx),  0.0f,
-		                 1 - 2*(xz + wy), 2*(yz + wx), 2*(xx - yy),  0.0f,
-		                            0.0f,        0.0f,        0.0f,  1.0f);
-
-		return Transform(transMat, transMat.Transpose());
-	}
-
-	float Quaternion::dot(const Quaternion &Q1, const Quaternion &Q2)
-	{
-		return vec4::Dot(Q1.qVector, Q2.qVector);
-	}
-
-	Quaternion Quaternion::slerp(const Quaternion &Q1, const Quaternion &Q2, float t)
-	{
-		float cosTheta = dot(Q1, Q2);
-		
-		if (cosTheta > .9995f)
-		{
-			return (Q1*(1.0f - t) + Q2*t).Normalize();
-		}
-		else
-		{
-			float theta = acosf(clamp(cosTheta, -1.0f, 1.0f));
-			float thetap = theta * t;
-			Quaternion qperp = (Q2 - Q1*cosTheta).Normalize();
-			return Q1 * cosf(thetap) + qperp * sinf(thetap);
-		}
-	}
 }
 
 #endif // _ENGINE_QUATERNION_H_
---
+++ src/Engine/Core/Quaternion.cpp
@@ -0,0 +1,122 @@
+#include <Engine/Core/Quaternion.h>
+
+namespace Engine
+{
+	Quaternion::Quaternion()
+		: mVec(0.0f, 0.0f, 0.0f), mW(1.0f)
+	{
+	}
+
+	Quaternion::Quaternion(const vec3 &v, float w)
+		: mVec(v), mW(w)
+	{
+	}
+
+	Quaternion::Quaternion(const Quaternion &Other)
+		: mVec(Other.mVec), mW(Other.mW)
+	{
+	}
+
+	Quaternion Quaternion::operator+(const Quaternion &Other) const
+	{
+		Quaternion ret(*this);
+		ret.mVec += Other.mVec;
+		ret.mW += Other.mW;
+		return ret;
+	}
+
+	Quaternion& Quaternion::operator+=(const Quaternion &Other)
+	{
+		mVec += Other.mVec;
+		mW += Other.mW;
+		return *this;
+	}
+
+	Quaternion Quaternion::operator-(const Quaternion &Other) const
+	{
+		Quaternion ret(*this);
+		ret.mVec -= Other.mVec;
+		ret.mW -= Other.mW;
+		return ret;
+	}
+
+	Quaternion& Quaternion::operator-=(const Quaternion &Other)
+	{
+		mVec -= Other.mVec;
+		mW -= Other.mW;
+		return *this;
+	}
+
+	Quaternion Quaternion::operator*(float f) const
+	{
+		Quaternion ret(*this);
+		ret.mVec *= f;
+		ret.mW *= f;
+		return ret;
+	}
+
+	Quaternion& Quaternion::operator*=(float f)
+	{
+		mVec *= f;
+		mW *= f;
+		return *this;
+	}
+
+	Quaternion Quaternion::operator/(float f) const
+	{
+		Quaternion ret(*this);
+		ret.mVec /= f;
+		ret.mW /= f;
+		return ret;
+	}
+
+	Quaternion& Quaternion::operator/=(float f)
+	{
+		mVec /= f;
+		mW /= f;
+		return *this;
+	}
+
+	Quaternion Quaternion::normalize() const
+	{
+		return *this / sqrtf(dot(mVec, mVec));
+	}
+
+	Transform Quaternion::toTransform() const
+	{
+		vec4 sq = mVec*mVec;
+
+		float xx = sq[0], yy = sq[1], zz = sq[2];
+		float xy = mVec[0] * mVec[1], xz = mVec[0] * mVec[2], yz = mVec[1] * mVec[2];
+		float wx = mVec[0] * mVec[3], wy = mVec[1] * mVec[3], wz = mVec[2] * mVec[3];
+
+		mat4x4 transMat(1 - 2 * (yy + zz), 2 * (xy + wz), 2 * (xz - wy), 0.0f,
+		                1 - 2 * (xy + wz), 2 * (xx + zz), 2 * (yz - wx), 0.0f,
+		                1 - 2 * (xz + wy), 2 * (yz + wx), 2 * (xx - yy), 0.0f,
+		                0.0f,              0.0f,          0.0f,          1.0f);
+
+		return Transform(transMat, transMat.Transpose());
+	}
+
+	float Quaternion::dot(const Quaternion &Q1, const Quaternion &Q2)
+	{
+		return vec3::Dot(Q1.mVec, Q2.mVec) + Q1.mW * Q2.mW;
+	}
+
+	Quaternion Quaternion::slerp(const Quaternion &Q1, const Quaternion &Q2, float t)
+	{
+		float cosTheta = dot(Q1, Q2);
+
+		if (cosTheta > .9995f)
+		{
+			return (Q1*(1.0f - t) + Q2*t).normalize();
+		}
+		else
+		{
+			float theta = acosf(clamp(cosTheta, -1.0f, 1.0f));
+			float thetap = theta * t;
+			Quaternion qperp = (Q2 - Q1*cosTheta).normalize();
+			return Q1 * cosf(thetap) + qperp * sinf(thetap);
+		}
+	}
+}
