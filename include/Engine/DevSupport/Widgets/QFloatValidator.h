6c419b17707624c906646f5f0aba2db144010ffd
Aaron Karp <aaron.m.karp@gmail.com>
2015-10-09 00:10:29 -0400

    Fixed some comment formatting in QFloatValidator.h.

--- include/Engine/DevSupport/Widgets/QFloatValidator.h
+++ include/Engine/DevSupport/Widgets/QFloatValidator.h
@@ -1,7 +1,7 @@
 #ifndef _ENGINE_Q_FLOAT_VALIDATOR_H_
 #define _ENGINE_Q_FLOAT_VALIDATOR_H_
 
-#include <Engine/Config.h>
+#include "../../Config.h"
 
 #ifdef ENGINE_DEVEOPMENT_SUPPORT
 
@@ -11,53 +11,53 @@ namespace Engine
 {
 
 	/**
-	* Validator for floating point textual input.
-	*/
+	 * Validator for floating point textual input.
+	 */
 	class ENGINE_DYNAMIC_CLASS QFloatValidator : public QValidator
 	{
 	public:
 		QFloatValidator(QObject *parent = 0);
 
 		/**
-		* Sets the minimum acceptable value.  If this is greater than the current
-		* value, the value is adjusted and the valueChanged() signal is emitted.
-		*
-		* @param Min
-		* 	The minimum value to accept.
-		*/
+		 * Sets the minimum acceptable value.  If this is greater than the current
+		 * value, the value is adjusted and the valueChanged() signal is emitted.
+		 *
+		 * @param Min
+		 * 	The minimum value to accept.
+		 */
 		void setMin(float Min);
 
 		/**
-		* Sets the maximum acceptable value.  If this is less than the current
-		* value, the value is adjusted and the valueChanged() signal is emitted.
-		*
-		* @param Max
-		* 	The maximum value to accept.
-		*/
+		 * Sets the maximum acceptable value.  If this is less than the current
+		 * value, the value is adjusted and the valueChanged() signal is emitted.
+		 *
+		 * @param Max
+		 * 	The maximum value to accept.
+		 */
 		void setMax(float Max);
 
 		/**
-		* Set the acceptable input range.  If the current value is outside of this
-		* range, the value is adjusted and the valueChanged() signal is emitted.
-		*
-		* @param Min
-		* 	The minimum value to accept.
-		*
-		* @param Max
-		* 	The maximum value to accept.
-		*/
+		 * Set the acceptable input range.  If the current value is outside of this
+		 * range, the value is adjusted and the valueChanged() signal is emitted.
+		 *
+		 * @param Min
+		 * 	The minimum value to accept.
+		 *
+		 * @param Max
+		 * 	The maximum value to accept.
+		 */
 		void setBounds(float Min, float Max);
 
 		/**
-		* @return
-		* 	The minimum acceptable value.
-		*/
+		 * @return
+		 * 	The minimum acceptable value.
+		 */
 		float getMin() const;
 
 		/**
-		* @return
-		* 	The maximum acceptable value.
-		*/
+		 * @return
+		 * 	The maximum acceptable value.
+		 */
 		float getMax() const;
 
 		virtual State validate(QString &input, int &pos) const;
